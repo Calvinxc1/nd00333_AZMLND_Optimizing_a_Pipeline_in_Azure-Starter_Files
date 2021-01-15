38c09b8a322e0e6a6afadf75ec8e7dd70d6437ef
Jason M. Cherry <jcherry@gmail.com>
2021-01-15 08:07:11 -0800

    Project completion

--- train.py
+++ train.py
@@ -10,19 +10,10 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
+
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +40,8 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df, y_df
+
 
 def main():
     # Add arguments to script
@@ -67,6 +59,23 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+ds = TabularDatasetFactory.from_delimited_files(
+    'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+)
+
+# +
+split = 0.8
+
+x, y = clean_data(ds)
+train_mask = x.index.isin(np.random.choice(x.index, size=int(x.index.size * split)))
+x_train, y_train = x.loc[train_mask], y.loc[train_mask]
+x_test, y_test  =x.loc[~train_mask], y.loc[~train_mask]
+# -
+
+# TODO: Split data into train and test sets.
+
+run = Run.get_context()
 
 if __name__ == '__main__':
     main()
